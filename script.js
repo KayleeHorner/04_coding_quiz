3500881620aab69ebf2dd43f3a9575d528135760
Kaylee Horner <kayleeahorner@gmail.com>
2020-10-05 22:22:53 -0500

    added start quiz function to button

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
+var startQuiz = document.querySelector(".start");
 var timer = document.querySelector(".timer");
 var main = document.querySelector(".image");
 
-var secondsLeft = 10;
+var secondsLeft = 90;
 
 function setTime() {
+
     var timerInterval = setInterval(function() {
         secondsLeft--;
         timer.textContent = "Time left: " + secondsLeft;
         
         if(secondsLeft === 0) {
-            clearInterval(timerInterval);
+            clearTimeout(timerInterval);
             sendMessage();
         }
   
     }, 1000);
 }
+    
 
 function sendMessage() {
     timer.textContent = " ";
@@ -26,4 +29,5 @@ function sendMessage() {
 
 }
 
-setTime();
+
+
